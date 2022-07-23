95bf6c771068033f166bf355c25cd8e5d82e0ce9
Vadim.Razzhivin <Vadimir14@gmail.com>
2022-07-23 17:15:56 +0300

    23.07 Thread(dbDao)

--- src/com/library/db/DatabaseDao.kt
+++ src/com/library/db/DatabaseDao.kt
@@ -5,42 +5,59 @@ import com.library.db.entities.ClientEntity
 import com.library.db.entities.OwnershipEntity
 import com.library.db.entities.OwnershipEntity.Companion.assign
 import com.library.utills.checkNotAMainThread
+import kotlin.concurrent.thread
 
 class DatabaseDao(private val database: Database) {
 
     fun getAllClients(): List<ClientEntity> {
-        checkNotAMainThread()
-        return database.clients
+        var clients = mutableListOf<ClientEntity>()
+        thread {
+            checkNotAMainThread()
+            clients = database.clients
+        }.join()
+        return clients
     }
 
     fun getAllBooks(): List<BookEntity> {
-        checkNotAMainThread()
-        return database.books
+        var books = mutableListOf<BookEntity>()
+        thread {
+            checkNotAMainThread()
+            books = database.books
+        }.join()
+        return books
     }
 
     fun getOwnershipInfo(): List<OwnershipEntity> {
-        checkNotAMainThread()
-        return database.ownership
+        var ownership = mutableListOf<OwnershipEntity>()
+        thread {
+            checkNotAMainThread()
+            ownership = database.ownership
+        }.join()
+        return ownership
     }
 
     fun addBookToClient(clientId: String, bookId: String) {
-        checkNotAMainThread()
-        var ownership: OwnershipEntity? = database.ownership
-            .firstOrNull { it.client.id == clientId }
-        val book = database.books.first { it.id == bookId }
-        if (ownership != null) {
-            ownership.booksInUse.add(book)
-        } else {
-            val client = database.clients.first { it.id == clientId }
-            ownership = client assign book
-        }
-        database.ownership.add(ownership)
+        thread {
+            checkNotAMainThread()
+            var ownership: OwnershipEntity? = database.ownership
+                .firstOrNull { it.client.id == clientId }
+            val book = database.books.first { it.id == bookId }
+            if (ownership != null) {
+                ownership.booksInUse.add(book)
+            } else {
+                val client = database.clients.first { it.id == clientId }
+                ownership = client assign book
+            }
+            database.ownership.add(ownership)
+        }.join()
     }
 
     fun confiscateBooks(clientId: String) {
-        checkNotAMainThread()
-        database.ownership.firstOrNull { it.client.id == clientId }?.let {
-            database.ownership.remove(it)
-        }
+        thread {
+            checkNotAMainThread()
+            database.ownership.firstOrNull { it.client.id == clientId }?.let {
+                database.ownership.remove(it)
+            }
+        }.join()
     }
 }
